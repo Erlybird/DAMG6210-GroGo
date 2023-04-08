96fdbfe5da9e384b469a9ef5c78137550fbe9179
Akhil Malladi <114646880+SriVenkataSatyaAkhilMalladi@users.noreply.github.com>
2023-04-07 21:02:41 -0400

    Add files via upload

---
+++ Queries/Drop_Tables.sql
@@ -0,0 +1,53 @@
+USE GROUP14_DMDD_TEST;
+--DeliveryBoyRatings
+DROP TABLE DeliveryBoyRatings;
+
+--Order Table
+DROP TABLE OrderTable;
+
+--Bill Table
+DROP TABLE Bill;
+
+--CartItem Table
+DROP TABLE CartItem;
+
+--BranchProduct Table
+DROP TABLE BranchProduct;
+
+--Product Table
+DROP TABLE Product;
+
+--ProductCategory Table
+DROP TABLE ProductCategory;
+
+--BranchPromo Table
+DROP TABLE BranchPromo;
+
+--Promotion Table
+DROP TABLE Promotion;
+
+--Cart Table
+DROP TABLE Cart;
+
+--DeliveryBoy Table
+DROP TABLE DeliveryBoy;
+
+--Customer Table
+DROP TABLE Customer;
+
+--PaymentCard Table
+DROP TABLE PaymentCard;
+
+--Branch Table
+DROP TABLE Branch;
+
+--Manager Table
+DROP TABLE Manager;
+
+--Address Table
+DROP TABLE Address;
+
+--Userdetails
+DROP TABLE userdetails;
+--Person Table
+DROP TABLE Person;
---
+++ Queries/Drop_procedures.sql
@@ -0,0 +1,14 @@
+
+USE GROUP14_DMDD_TEST;
+
+DROP PROCEDURE sp_InsertPerson;
+
+DROP PROCEDURE sp_InsertAddress;
+
+DROP PROCEDURE sp_InsertManager;
+
+DROP PROCEDURE sp_InsertBranch;
+
+DROP PROCEDURE sp_InsertPaymentCard;
+
+DROP PROCEDURE sp_InsertCustomer;
